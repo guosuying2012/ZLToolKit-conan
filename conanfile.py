2dbaba245b51da2ad36f92022631c6c413ff0c02
yengsu <yengsu@yengsu.com>
2021-02-07 15:26:13 +0800

    解决替换函数参数错误

--- conanfile.py
+++ conanfile.py
@@ -32,13 +32,13 @@ include(${CMAKE_BINARY_DIR}/conan_paths.cmake)
 set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR} ${CMAKE_MODULE_PATH})''')
 
         if self.options.shared:
-        	tools.replace_in_file("ZLToolKit/CMakeLists.txt", ''' add_library(${CMAKE_PROJECT_NAME}_shared SHARED ${SRC_LIST})
+        	tools.replace_in_file("ZLToolKit/CMakeLists.txt", "add_library(${CMAKE_PROJECT_NAME}_shared SHARED ${SRC_LIST})
     target_link_libraries(${CMAKE_PROJECT_NAME}_shared ${LINK_LIB_LIST})
-    set_target_properties(${CMAKE_PROJECT_NAME}_shared PROPERTIES OUTPUT_NAME "${CMAKE_PROJECT_NAME}")
-    install(TARGETS ${CMAKE_PROJECT_NAME}_shared  ARCHIVE DESTINATION ${INSTALL_PATH_LIB} LIBRARY DESTINATION ${INSTALL_PATH_LIB}) ''', '''  ''')
+    set_target_properties(${CMAKE_PROJECT_NAME}_shared PROPERTIES OUTPUT_NAME '${CMAKE_PROJECT_NAME}')
+    install(TARGETS ${CMAKE_PROJECT_NAME}_shared  ARCHIVE DESTINATION ${INSTALL_PATH_LIB} LIBRARY DESTINATION ${INSTALL_PATH_LIB})", '''  ''')
         else:
-        	tools.replace_in_file("ZLToolKit/CMakeLists.txt", ''' add_library(${CMAKE_PROJECT_NAME}_static STATIC ${SRC_LIST})
-set_target_properties(${CMAKE_PROJECT_NAME}_static PROPERTIES OUTPUT_NAME "${CMAKE_PROJECT_NAME}") ''', '''  ''')
+        	tools.replace_in_file("ZLToolKit/CMakeLists.txt", "add_library(${CMAKE_PROJECT_NAME}_static STATIC ${SRC_LIST})
+set_target_properties(${CMAKE_PROJECT_NAME}_static PROPERTIES OUTPUT_NAME '${CMAKE_PROJECT_NAME}')", '''  ''')
         	pass
 
     def build(self):
